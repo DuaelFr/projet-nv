b7c8bae0f4e9a39f97cf89c34c69e55bd6b36333
Edouard Cunibil <edouard@happyculture.coop>
2023-05-21 18:10:09 +0200

    Include @11ty/eleventy-img and create the image shortcode.

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,30 @@
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt = "", sizes = "100vw") {
+  let metadata = await Image(src, {
+    widths: [300, 600, "auto"],
+    formats: ["webp", "jpeg"],
+    outputDir: "./dist/img/",
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
 module.exports = function(eleventyConfig) {
   // Watch CSS files for changes
   eleventyConfig.setBrowserSyncConfig({
     files: './dist/css/**/*.css'
   });
+
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
 
   return {
     dir: {
